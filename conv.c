aae09599c2dae1a35260ecd379c6d0eaa47a5eaa
bob <patrick.g.langan@gmail.com>
2014-04-24 15:44:16 -0400

    Put arc and line calculation in subroutines

--- conv.c
+++ conv.c
@@ -112,7 +112,7 @@ struct vector getArcPos (struct arcInfo *arc, double angle)
         };
 }
 
-int getRampInfo (struct rampInfo *ramp, struct vector *start, struct vector *end, double startVel, double endVel, struct vector *line)
+int calcRampInfo (struct rampInfo *ramp, struct vector *start, struct vector *end, double startVel, double endVel, struct vector *line)
 {
         ramp->accel = (struct vector){DBL_MAX, DBL_MAX, DBL_MAX};
 
@@ -142,7 +142,99 @@ int getRampInfo (struct rampInfo *ramp, struct vector *start, struct vector *end
         return 0;
 }
 
-/* Reads the G-code file into an array */
+/*
+ * This subroutine calculates a travelable path based on the given G-code commands.
+ * The path's corners are rounded based on the mills maximum accelerations.
+ */
+void calcArcInfo (struct gCodeInfo *gCode, struct arcInfo *arc, struct lineInfo *line, int lineNum)
+{
+	unsigned int i;
+
+        for (i = 0; i < lineNum - 2; i++) {
+		struct vector toCenter;
+		struct vector normal;
+		struct vector start;
+		struct vector end;
+		double centerMag;
+		double length;
+		double theta;
+
+		double accel = DBL_MAX;
+
+		//start and end are unit vectors pointing to the last and next G-code points from the current one
+		start = subVec (&gCode[i].p, &gCode[i + 1].p);
+		normalize (&start);
+		end = subVec (&gCode[i + 2].p, &gCode[i + 1].p);
+		normalize (&end);
+
+		//toCenter is a unit vector pointing to where the center of the arc will be from the current G-code position
+		toCenter = addVec (&start, &end);
+		normalize (&toCenter);
+
+		//theta is half the angle between start and end
+		//angle is the angle that the arc will travel
+		theta = angleProduct (&start, &toCenter);
+		arc[i].angle = 2.0 * ((M_PI / 2.0) - theta);
+
+		//Getting the minimum allowed acceleration of the axies in motion
+		if (gCode[i + 1].p.x != gCode[i + 2].p.x && maxAccel.x < accel)
+			accel = maxAccel.x;
+		if (gCode[i + 1].p.y != gCode[i + 2].p.y && maxAccel.y < accel)
+			accel = maxAccel.y;
+		if (gCode[i + 1].p.z != gCode[i + 2].p.z && maxAccel.z < accel)
+			accel = maxAccel.y;
+
+		//Finding a radius that will not exceed the max acceleration, assuming the max accel is reached at the worst possible moment for the axis with the lowest allowed acceleration
+		//Simplify equation
+		arc[i].radius = sqrt (pow (((0.5 * pow (gCode[i + 2].f - gCode[i + 1].f, 2.0)) + ((gCode[i + 2].f - gCode[i + 1].f) * gCode[i + 1].f)) / (arc[i].angle * accel), 2.0) + pow (pow ((gCode[i + 2].f > gCode[i + 1].f)? gCode[i + 2].f : gCode[i + 1].f, 2.0) / accel, 2.0));
+
+		//Finding the time spent traveling on the arc
+		arc[i].time = (arc[i].angle * arc[i].radius) / (((gCode[i + 2].f - gCode[i + 1].f) / 2.0) + gCode[i + 1].f);
+
+		//Calculating the actual acceleration
+		arc[i].accel = (gCode[i + 2].f - gCode[i + 1].f) / arc[i].time;
+
+		//Converting the unit vector to the center to a vector at the center
+		centerMag = arc[i].radius / sin (theta);
+		arc[i].center = multVec (&toCenter, centerMag);
+
+		//Calculating the arc's start and end points
+		length = arc[i].radius / tan (theta);
+		line[i + 1].start = multVec (&end, length);
+		line[i].end = multVec (&start, length);
+
+		//Calculating a vector from the center to the start point
+		arc[i].toSurf = subVec (&line[i].end, &arc[i].center);
+		normalize (&arc[i].toSurf);
+
+		//Moving the vectors from 000 to the current G-code position
+		arc[i].center = addVec (&arc[i].center, &gCode[i + 1].p);
+		line[i + 1].start = addVec (&line[i + 1].start, &gCode[i + 1].p);
+		line[i].end = addVec (&line[i].end, &gCode[i + 1].p);
+
+		//Calculating the cross unit vector, needed for the 3D equation of a circle
+		normal = crossProduct (&end, &start);
+		arc[i].cross = crossProduct (&normal, &arc[i].toSurf);
+		normalize (&arc[i].cross);
+
+		//Multiplying the circle equation unit vectors by the radius, needed for the 3D equation of a circle
+		arc[i].toSurf = multVec (&arc[i].toSurf, arc[i].radius);
+		arc[i].cross = multVec (&arc[i].cross, arc[i].radius);
+	}
+}
+
+void calcLineInfo (struct gCodeInfo *gCode, struct lineInfo *line, int lineNum)
+{
+	unsigned int i;
+
+        for (i = 0; i < lineNum - 1; i++) {
+                line[i].velocity = subVec (&line[i].end, &line[i].start);
+                line[i].time = magnitude (&line[i].velocity) / gCode[i + 1].f;
+		normalize (&line[i].velocity);
+		line[i].velocity = multVec (&line[i].velocity, gCode[i + 1].f);
+        }
+}
+
 void readFile (char *fileName, struct gCodeInfo **gCode, int *lineNum)
 {
 	unsigned int i;
@@ -213,7 +305,7 @@ void readFile (char *fileName, struct gCodeInfo **gCode, int *lineNum)
         fclose (file);
 }
 
-void writeFile (struct rampInfo *ramp, struct arcInfo *arc, struct lineInfo *line, int *lineNum, char *fileName)
+void writeFile (struct rampInfo *ramp, struct arcInfo *arc, struct lineInfo *line, int lineNum, char *fileName)
 {
 	FILE *file;
 	char *name;
@@ -222,26 +314,21 @@ void writeFile (struct rampInfo *ramp, struct arcInfo *arc, struct lineInfo *lin
 
 	file = fopen (strcat (name, ".bin"), "w");
 
-	fwrite (lineNum, sizeof(int), 1, file);
+	fwrite (&lineNum, sizeof(int), 1, file);
 	fwrite (ramp, sizeof(struct rampInfo), 2, file);
-	fwrite (arc, sizeof(struct arcInfo), *lineNum - 2, file);
-	fwrite (line, sizeof(struct lineInfo), *lineNum - 1, file);
+	fwrite (arc, sizeof(struct arcInfo), lineNum - 2, file);
+	fwrite (line, sizeof(struct lineInfo), lineNum - 1, file);
 
 	fclose (file);
 }
 
-//Put each component into a subroutine
 int main (int argc, char **argv)
 {
-        //Clean up the variables
-        unsigned int i;
-        unsigned int lineNum;
         struct gCodeInfo *gCode;
-
-        struct arcInfo *arc;
-        struct lineInfo *line;
-
         struct rampInfo ramp[2];
+        struct lineInfo *line;
+        struct arcInfo *arc;
+        int lineNum;
 
 	if (argc < 2) {
 		fprintf (stderr, "You need to pass in a G-code file\n");
@@ -256,98 +343,15 @@ int main (int argc, char **argv)
         arc = malloc ((lineNum - 2) * sizeof(*arc));
         line = malloc ((lineNum - 1) * sizeof(*line));
 
-        //Clean up
-        //Shift i down one?
-        //Improve and add comments
-        for (i = 1; i < lineNum - 1; i++) {
-                struct vector toCenter;
-                struct vector normal;
-                struct vector start;
-                struct vector end;
-                double centerMag;
-                double length;
-                double theta;
+        calcRampInfo (&ramp[0], &gCode[0].p, &gCode[1].p, gCode[0].f, gCode[1].f, &line[0].start);
+        calcRampInfo (&ramp[1], &gCode[lineNum - 2].p, &gCode[lineNum - 1].p, gCode[lineNum - 1].f, 0.0, &line[lineNum - 2].end);
+	calcArcInfo (gCode, arc, line, lineNum);
+	calcLineInfo (gCode, line, lineNum);
 
-                double accel = DBL_MAX;
-
-                //start and end are unit vectors pointing to the last and next G-code points from the current one
-                start = subVec (&gCode[i - 1].p, &gCode[i].p);
-                end = subVec (&gCode[i + 1].p, &gCode[i].p);
-                normalize (&start);
-                normalize (&end);
-
-                //toCenter is a unit vector pointing to where the center of the arc will be from the current G-code position
-                toCenter = addVec (&start, &end);
-                normalize (&toCenter);
-
-                //theta is half the angle between start and end
-                //angle is the angle that the arc will travel
-                theta = angleProduct (&start, &toCenter);
-                arc[i - 1].angle = 2.0 * ((M_PI / 2.0) - theta);
-
-		//Getting the minimum allowed acceleration of the axies in motion
-                if (gCode[i].p.x != gCode[i + 1].p.x && maxAccel.x < accel)
-                        accel = maxAccel.x;
-                if (gCode[i].p.y != gCode[i + 1].p.y && maxAccel.y < accel)
-                        accel = maxAccel.y;
-                if (gCode[i].p.z != gCode[i + 1].p.z && maxAccel.z < accel)
-                        accel = maxAccel.y;
-
-                //Finding a radius that will not exceed the max acceleration, assuming the max accel is reached at the worst possible moment for the axis with the lowest allowed acceleration
-                //Simplify equation
-                arc[i - 1].radius = sqrt (pow (((0.5 * pow (gCode[i + 1].f - gCode[i].f, 2.0)) + ((gCode[i + 1].f - gCode[i].f) * gCode[i].f)) / (arc[i - 1].angle * accel), 2.0) + pow (pow ((gCode[i + 1].f > gCode[i].f)? gCode[i + 1].f : gCode[i].f, 2.0) / accel, 2.0));
-
-                //Finding the time spent traveling on the arc
-                arc[i - 1].time = (arc[i - 1].angle * arc[i - 1].radius) / (((gCode[i + 1].f - gCode[i].f) / 2.0) + gCode[i].f);
-
-		//Calculating the actual acceleration
-		arc[i - 1].accel = (gCode[i + 1].f - gCode[i].f) / arc[i - 1].time;
-
-                //Converting the unit vector to the center to a vector at the center
-                centerMag = arc[i - 1].radius / sin (theta);
-                arc[i - 1].center = multVec (&toCenter, centerMag);
-
-                //Calculating the arc's start and end points
-		length = arc[i - 1].radius / tan (theta);
-		line[i].start = multVec (&end, length);
-		line[i - 1].end = multVec (&start, length);
-
-		//Calculating a vector from the center to the start point
-		arc[i - 1].toSurf = subVec (&line[i - 1].end, &arc[i - 1].center);
-		normalize (&arc[i - 1].toSurf);
-
-		//Moving the vectors from 000 to the current G-code position
-		arc[i - 1].center = addVec (&arc[i - 1].center, &gCode[i].p);
-		line[i].start = addVec (&line[i].start, &gCode[i].p);
-		line[i - 1].end = addVec (&line[i - 1].end, &gCode[i].p);
-
-		//Calculating the cross unit vector, needed for the 3D equation of a circle
-		normal = crossProduct (&end, &start);
-		arc[i - 1].cross = crossProduct (&normal, &arc[i - 1].toSurf);
-		normalize (&arc[i - 1].cross);
-
-                //Multiplying the circle equation unit vectors by the radius, needed for the 3D equation of a circle
-                arc[i - 1].toSurf = multVec (&arc[i - 1].toSurf, arc[i - 1].radius);
-                arc[i - 1].cross = multVec (&arc[i - 1].cross, arc[i - 1].radius);
-        }
-
-        //Calculating acceleration, etc of ramp up and ramp down
-        getRampInfo (&ramp[0], &gCode[0].p, &gCode[1].p, gCode[0].f, gCode[1].f, &line[0].start);
-        getRampInfo (&ramp[1], &gCode[lineNum - 2].p, &gCode[lineNum - 1].p, gCode[lineNum - 1].f, 0.0, &line[lineNum - 2].end);
-
-        //finding the time spent traveling on each line, and a unit vector pointing the direction of travel
-        //Make more efficient
-        for (i = 0; i < lineNum - 1; i++) {
-                line[i].velocity = subVec (&line[i].end, &line[i].start);
-                line[i].time = magnitude (&line[i].velocity) / gCode[i + 1].f;
-		normalize (&line[i].velocity);
-		line[i].velocity = multVec (&line[i].velocity, gCode[i + 1].f);
-        }
-
-	writeFile (ramp, arc, line, &lineNum, argv[1]);
-
-        //Free as soon a possible
         free (gCode);
+
+	writeFile (ramp, arc, line, lineNum, argv[1]);
+
         free (arc);
         free (line);
 
